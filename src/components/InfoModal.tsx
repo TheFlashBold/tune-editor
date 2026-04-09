68eda065f066763899bf358faedb3830ee0fe529
Tycho Holzer <tycho.holzer@gmail.com>
2026-04-09 14:22:55 +0200

    1.19.1

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -20,7 +20,7 @@ const tips: string[] = [
 
 const changelog: ChangelogEntry[] = [
     {
-        version: '1.19.0',
+        version: '1.19.1',
         date: '09.04.2026',
         changes: [
             'Added many new SCB, SCG, SC8 definitions',
